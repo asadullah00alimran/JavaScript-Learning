660c85c7de0e36838ed5ab58574aff20440a0741
Asadullah Al Imran <asadullahalimran00@gmail.com>
2021-09-06 11:49:38 +0600

    Converting Data Types

---
+++ 5th_code.js
@@ -0,0 +1,23 @@
+//Convert data type to srting
+var num = 20;
+num = toString(num);
+console.log(typeof (num));
+
+
+//Convert data type to integer
+var num = 234;
+num = parseInt(num);
+console.log(typeof (num));
+
+//Convert data type to Float number
+var num = 23.500;
+num = parseFloat(num);
+console.log(typeof (num));
+
+var number = 23.45434543;
+console.log(number.toFixed(1)); //Round fix after deci,al point
+
+var number = 23.45434543;
+//parameter shows total no.of digit after and before decimal point
+console.log(number.toPrecision(5)); 
+
